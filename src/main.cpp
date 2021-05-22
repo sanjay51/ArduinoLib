cb382714415f24d7d3d998e2920080a730316949
sanjav <sanjayvermaseattle@gmail.com>
2021-05-21 20:52:19 -0700

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,13 @@
 #include <Joystick.h>
 
 ServoMotor servo(9);
+Joystick joystick(1, 2);
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   servo.init();
 }
 
-
-Joystick joystick(1, 2);
 void loop() {
   switch (joystick.getDirection())
   {
